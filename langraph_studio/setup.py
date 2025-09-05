911c29c8830a36739f3fa7af9e3399c6ec38da2f
Chloe Aerts <chloe.aerts@zestforleaders.com>
2025-09-05 18:50:45 +0200

    supprime setup.py

--- langraph_studio/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="langraph-studio",
-    version="0.1.0",
-    packages=find_packages(),
-    py_modules=["zest_companion_workflow_modular", "zest_companion_workflow", "zest_mbti_workflow"],
-    include_package_data=True,
-)
